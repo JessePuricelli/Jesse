4bbcf51991e896318812bd1227161061b50966fb
Jesse Puricelli <JDPuricelli@outlook.com>
2024-09-21 15:02:28 -0400

    Update ContactForm.tsx
    Signed-off-by: Jesse Puricelli <JDPuricelli@outlook.com>

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -1,4 +1,5 @@
-import {FC, memo, useCallback, useMemo, useState} from 'react';
+import React, { FC, memo, useCallback, useMemo } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
 interface FormData {
   name: string;
@@ -7,71 +8,61 @@ interface FormData {
 }
 
 const ContactForm: FC = memo(() => {
+  const [state, handleSubmit] = useForm("mdknrdnl");
+
   const defaultData = useMemo(
     () => ({
-      name: 'Jesse Puricelli',
-      email: 'JDPuricelli@outlook.com',
+      name: '',
+      email: '',
       message: '',
     }),
-    [],
-  );
-
-  const [data, setData] = useState<FormData>(defaultData);
-
-  const onChange = useCallback(
-    <T extends HTMLInputElement | HTMLTextAreaElement>(event: React.ChangeEvent<T>): void => {
-      const {name, value} = event.target;
-
-      const fieldData: Partial<FormData> = {[name]: value};
-
-      setData({...data, ...fieldData});
-    },
-    [data],
-  );
-
-  const handleSendMessage = useCallback(
-    async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      /**
-       * This is a good starting point to wire up your form submission logic
-       * */
-
-     
-
-    
-      console.log('Data to send: ', data);
-    },
-    [data],
+    []
   );
 
   const inputClasses =
     'bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm';
 
+  if (state.succeeded) {
+    return <p className="text-green-500">Thanks for your message! We'll get back to you soon.</p>;
+  }
+
   return (
-    <form className="grid min-h-[320px] grid-cols-1 gap-y-4" method="POST" onSubmit={handleSendMessage}>
-      <input className={inputClasses} name="name" onChange={onChange} placeholder="Name" required type="text" />
+    <form className="grid min-h-[320px] grid-cols-1 gap-y-4" onSubmit={handleSubmit}>
+      <input 
+        className={inputClasses} 
+        name="name" 
+        placeholder="Name" 
+        required 
+        type="text" 
+      />
+      <ValidationError prefix="Name" field="name" errors={state.errors} />
+      
       <input
         autoComplete="email"
         className={inputClasses}
         name="email"
-        onChange={onChange}
         placeholder="Email"
         required
         type="email"
       />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      
       <textarea
         className={inputClasses}
         maxLength={250}
         name="message"
-        onChange={onChange}
         placeholder="Message"
         required
         rows={6}
       />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      
       <button
         aria-label="Submit contact form"
         className="w-max rounded-full border-2 border-orange-600 bg-stone-900 px-4 py-2 text-sm font-medium text-white shadow-md outline-none hover:bg-stone-800 focus:ring-2 focus:ring-orange-600 focus:ring-offset-2 focus:ring-offset-stone-800"
-        type="submit">
+        type="submit"
+        disabled={state.submitting}
+      >
         Send Message
       </button>
     </form>

4fbb9f3d36f8d991d8b16fc5b9bab1da501739c7
Jesse Puricelli <JDPuricelli@outlook.com>
2024-09-26 11:58:25 -0400

    Update HandShakeIcon.tsx
    Signed-off-by: Jesse Puricelli <JDPuricelli@outlook.com>

--- src/components/Icon/HandShakeIcon.tsx
+++ src/components/Icon/HandShakeIcon.tsx
@@ -1,12 +1,10 @@
 import {FC, memo} from 'react';
-
 import Icon, {IconProps} from './Icon';
-
 
 const HandShakeIcon: FC<IconProps> = memo(props => (
   <Icon viewBox="0 0 24 24" {...props}>
-    <rect width="24" height="24" rx="4" ry="4" fill="currentColor"/>
-    <path d="M6 6v12h3v-5h6v5h3V6h-3v5H9V6H6z" fill="white"/>
+    <rect x="2" y="2" width="20" height="21" rx="4" ry="4" fill="currentColor"/>
+    <path d="M7 5v14h2v-6h6v6h2V5h-2v6H9V5H7z" fill="Black"/>
   </Icon>
 ));
 

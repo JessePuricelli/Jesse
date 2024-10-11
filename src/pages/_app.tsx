af8d1f8459b607b8a503a8c30ec9a495d767185c
Jesse Puricelli <JDPuricelli@outlook.com>
2024-10-11 12:17:14 -0400

    Update _app.tsx
    Signed-off-by: Jesse Puricelli <JDPuricelli@outlook.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
-
-import type {AppProps} from 'next/app';
-import {memo} from 'react';
-
-const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
-});
-
-export default MyApp;
-
 import type { AppProps } from 'next/app';
+import { memo } from 'react';
 import { Analytics } from "@vercel/analytics/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = memo(({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
     </>
   );
-}
+});
 
 export default MyApp;

8a28d703f774d3ccf8036fe3614607b195332a1f
Jesse Puricelli <JDPuricelli@outlook.com>
2024-10-11 19:46:34 -0400

    Update Skills.tsx
    Signed-off-by: Jesse Puricelli <JDPuricelli@outlook.com>

--- src/components/Sections/Resume/Skills.tsx
+++ src/components/Sections/Resume/Skills.tsx
@@ -1,8 +1,9 @@
-import {FC, memo, PropsWithChildren} from 'react';
-import {Skill as SkillType, SkillGroup as SkillGroupType} from '../../../data/dataDef';
+import { FC, memo, PropsWithChildren } from 'react';
 
-export const SkillGroup: FC<PropsWithChildren<{skillGroup: SkillGroupType}>> = memo(({skillGroup}) => {
-  const {name, skills} = skillGroup;
+import { Skill as SkillType, SkillGroup as SkillGroupType } from '../../../data/dataDef';
+
+export const SkillGroup: FC<PropsWithChildren<{ skillGroup: SkillGroupType }>> = memo(({ skillGroup }) => {
+  const { name, skills } = skillGroup;
   return (
     <div className="flex flex-col">
       <span className="text-center text-lg font-bold mb-2">{name}</span>
@@ -17,8 +18,8 @@ export const SkillGroup: FC<PropsWithChildren<{skillGroup: SkillGroupType}>> = m
 
 SkillGroup.displayName = 'SkillGroup';
 
-export const Skill: FC<{skill: SkillType}> = memo(({skill}) => {
-  const {name} = skill;
+export const Skill: FC<{ skill: SkillType }> = memo(({ skill }) => {
+  const { name } = skill;
   return (
     <div className="bg-neutral-300 px-3 py-1 rounded-full text-sm font-medium">
       {name}
